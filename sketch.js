bb4bc2736061530c495a2db79427985538d981e4
Reetinder1 <70577876+Reetinder1@users.noreply.github.com>
2020-09-16 21:09:55 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -43,7 +43,7 @@ function setup() {
 	
 	sideBody1 = new Box(300, 610, 20, 100, {isStatic:true} );
 	
-	sideBody2 = new Box(100, 610, 20, 100, {isStatic:true} );
+	sideBody2 = new Box(500, 610, 20, 100, {isStatic:true} );
 	
 	downBody = new Box(400, 635, 200, 20, {isStatic:true} );
 	
