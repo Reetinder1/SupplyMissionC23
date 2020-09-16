0bd10f8e74b1738386939d7fbdda808d01d6dcad
Reetinder1 <70577876+Reetinder1@users.noreply.github.com>
2020-09-16 18:09:28 +0530

    Update Box.js

--- Box.js
+++ Box.js
@@ -1,7 +1,7 @@
 class Box {
     constructor(x, y, width, height) {
       var options = {
-          'restitution':0.8,
+          //'restitution':0.8,
           //'friction':0.3,
           //'density':1.0
       }
