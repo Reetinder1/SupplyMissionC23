232994f7044430c89c4321ce09a1132734c43513
Reetinder1 <70577876+Reetinder1@users.noreply.github.com>
2020-11-22 13:08:58 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
 var packageBody,ground;
-var downBody,sideBody1,sideBody2;
+var log1,log2,log3;
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
@@ -13,7 +13,10 @@ function preload()
 }
 
 function setup() {
-	createCanvas(800, 700);
+	createCanvas(800, 600);
+
+	engine = Engine.create();
+    world = engine.world;
 	rectMode(CENTER);
 	
 
@@ -28,61 +31,53 @@ function setup() {
 	groundSprite=createSprite(width/2, height-35, width,10);
 	groundSprite.shapeColor=color(255)
 
+	
 
-	engine = Engine.create();
-	world = engine.world;
+	log1= new Box(400,550,200,20);
+	log2= new Box(510,510,20,100);
+	log3= new Box(298,510,20,100);
+	
 
-	packageBody = Bodies.circle(width/2, 200 , 5 , {restitution:0.3, isStatic:true});
+
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.2, isStatic:true});
 	World.add(world, packageBody);
 	
 
 	//Create a Ground
-	ground = Bodies.rectangle(width/4, 650, width, 10 , {isStatic:true} );
+	ground = Bodies.rectangle(width/2, height-70, width, 10 , {isStatic:true} );
  	World.add(world, ground);
+
+
 	
-	downBody = new Box(400, 635, 200, 20, {isStatic:true} );
-	
-	sideBody2 = new Box(500, 610, 20, 100, {isStatic:true} );
-	
-	sideBody1 = new Box(300,640, 20, 100, {isStatic:true} );
-	
-	Engine.run(engine);
   
 }
 
 
 function draw() {
-  rectMode(CENTER);
-  background(0);
-  packageSprite.x= packageBody.position.x 
-  packageSprite.y= packageBody.position.y 
+	background(0);
+	Engine.update(engine);
+    rectMode(CENTER);
   
-  if(keyCode === LEFT_ARROW){
-    helicopterSprite.x = helicopterSprite.x-10; 
-  }	
-  if(keyCode === RIGHT_ARROW){
-   helicopterSprite.x = helicopterSprite.x+10;   
-  }	
-	
-  
-	
-  downBody.display();
-  sideBody1.display();
-  sideBody2.display();
-  drawSprites();
+ 
+    packageSprite.x= packageBody.position.x 
+    packageSprite.y= packageBody.position.y 
 
+    log1.display();
+    log2.display();
+	log3.display();
+	
+    keyPressed();
+    drawSprites();
+ 
 }
+
 
 function keyPressed() {
-	
-  if (keyCode === DOWN_ARROW) {
-    // Look at the hints in the document and understand how to make the package body fall only on
-    Matter.Body.setStatic(packageBody,false);
-  
-   
-     
-  }
-}
+if (keyCode === DOWN_ARROW) {
+	Matter.Body.setStatic(packageBody, false);
 
+}
+}
 
 
